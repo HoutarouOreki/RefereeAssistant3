7d34b029e8c13ee145f1ed952ea106e986c185d1
Hōtarō Oreki <TheZjarek@gmail.com>
2019-06-25 17:18:16 +0200

    Add basic interface

--- RefereeAssistant3.Visual/RefereeAssistant3Visual.cs
+++ RefereeAssistant3.Visual/RefereeAssistant3Visual.cs
@@ -1,5 +1,8 @@
 ﻿using osu.Framework;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.UserInterface;
+using osuTK;
 using RefereeAssistant3.Main;
 using System.Collections.Generic;
 
@@ -8,16 +11,56 @@ namespace RefereeAssistant3.Visual
     public class RefereeAssistant3Visual : Game
     {
         private readonly List<Match> matches = new List<Match>();
+        private TextBox player1TextBox;
+        private TextBox player2TextBox;
+        public const float COMPONENTS_WIDTH = 100;
+        public const float COMPONENTS_HEIGHT = 30;
+        public const float SPACING = 24;
 
-        public RefereeAssistant3Visual()
+        protected override void LoadComplete()
         {
+            base.LoadComplete();
             Children = new Drawable[]
             {
+                new FillFlowContainer
+                {
+                    AutoSizeAxes = Axes.Both,
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Spacing = new Vector2(SPACING),
+                    Children = new Drawable[]
+                    {
+                        player1TextBox = new BasicTextBox
+                        {
+                            PlaceholderText = "Player 1 username",
+                            Width = COMPONENTS_WIDTH,
+                            Height = COMPONENTS_HEIGHT,
+                        },
+                        player2TextBox = new BasicTextBox
+                        {
+                            PlaceholderText = "Player 2 username",
+                            Width = COMPONENTS_WIDTH,
+                            Height = COMPONENTS_HEIGHT,
+                        },
+                        new Button
+                        {
+                            Text = "Add match",
+                            Action = AddNewMatch,
+                            Width = COMPONENTS_WIDTH,
+                            Height = COMPONENTS_HEIGHT,
+                            BackgroundColour = FrameworkColour.Green,
+                        }
+                    }
+                }
             };
         }
 
         private void AddNewMatch()
         {
+            var player1 = new Player(player1TextBox.Text);
+            var player2 = new Player(player2TextBox.Text);
+            var match = new Match(new[] { player1, player2 }, new Mappool(), TournamentStage.Qualifiers);
+            matches.Add(match);
         }
     }
 }
