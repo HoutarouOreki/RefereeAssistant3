c2be6ea55ac33a202aef4ff1cb13e676c909ad55
Hōtarō Oreki <TheZjarek@gmail.com>
2019-06-27 16:43:52 +0200

    Adjust match procedure roll dependent handling

--- RefereeAssistant3.Main/Match.cs
+++ RefereeAssistant3.Main/Match.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 
 namespace RefereeAssistant3.Main
 {
@@ -19,36 +18,20 @@ namespace RefereeAssistant3.Main
         public readonly TournamentStage TournamentStage;
 
         public List<MatchProcedure> Procedures = new List<MatchProcedure>();
+        private readonly ProcedureUtilities procedureUtilities;
 
         public MatchProcedure CurrentProcedure => Procedures[currentProcedureIndex];
         private int currentProcedureIndex;
 
         public Team RollWinner;
-        public Team RollLoser => Team1 == RollWinner ? Team2 : Team1;
+        public Team RollLoser => RollWinner == null ? null :
+            Team1 == RollWinner ? Team2 : Team1;
 
         public Map SelectedMap;
 
         public Dictionary<Team, int> Score = new Dictionary<Team, int>();
 
         public readonly Dictionary<Map, Dictionary<Player, int>> MapResults = new Dictionary<Map, Dictionary<Player, int>>();
-
-        // after warmups, picks and tiebreaker a map is played
-        private Dictionary<MatchProcedure, string> readableMatchStateDictionary => new Dictionary<MatchProcedure, string>
-        {
-            { MatchProcedure.SettingUp, "Setting up the match" },
-            { MatchProcedure.WarmUp1, $"Warmup by {Team1}" },
-            { MatchProcedure.WarmUp2, $"Warmup by {Team2}" },
-            { MatchProcedure.Rolling, $"Teams are rolling" },
-            { MatchProcedure.Banning1, $"{Team1} are banning" },
-            { MatchProcedure.Banning2, $"{Team2} are banning" },
-            { MatchProcedure.Picking1, $"{Team1} are picking" },
-            { MatchProcedure.Picking2, $"{Team2} are picking" },
-            { MatchProcedure.GettingReady, "Players are getting ready" },
-            { MatchProcedure.TieBreaker, "Tiebreaker!" },
-            { MatchProcedure.Playing, $"Playing {SelectedMap?.MapCode}: {SelectedMap?.DisplayName}" },
-            { MatchProcedure.FreePoint1, $"{Team1} receives a free point" },
-            { MatchProcedure.FreePoint2, $"{Team2} receives a free point" }
-        };
 
         public string Title => TournamentStage.RoomName.Replace("TEAM1", Team1.TeamName).Replace("TEAM2", Team2.TeamName);
 
@@ -62,32 +45,92 @@ namespace RefereeAssistant3.Main
             Tournament = tournament;
             TournamentStage = tournamentStage;
 
+            procedureUtilities = new ProcedureUtilities(this);
+
             Procedures.Add(MatchProcedure.SettingUp);
-            foreach (var procedureParameter in tournamentStage.MatchProceedings)
-                GenerateMatchProcedure(procedureParameter.ToLowerInvariant());
+            GenerateMatchProcedures();
         }
 
-        public string ReadableCurrentState => readableMatchStateDictionary[CurrentProcedure];
+        private void GenerateMatchProcedures()
+        {
+            Procedures.Clear();
+            foreach (var procedureParameter in TournamentStage.MatchProceedings)
+                GenerateMatchProcedure(procedureParameter);
+            foreach (var procedure in Procedures)
+                Console.WriteLine($"{procedure}: {procedureUtilities.GetProcedureDescription(procedure)}");
+        }
+
+        public string ReadableCurrentState => procedureUtilities.GetProcedureDescription(CurrentProcedure);
 
         private void GenerateMatchProcedure(string param)
         {
-            if (warmup_regex.IsMatch(param))
-                GenerateWarmupProcedure(TeamFromLetter(warmup_regex.Match(param).Groups[1].Value));
-            else if (roll_regex.IsMatch(param))
-                GenerateRollingProcedure();
-            else if (pick_regex.IsMatch(param))
-                GeneratePickingProcedure(TeamFromLetter(pick_regex.Match(param).Groups[1].Value));
-            else if (ban_regex.IsMatch(param))
-                GenerateBanningProcedure(TeamFromLetter(ban_regex.Match(param).Groups[1].Value));
-            else if (tiebreaker_regex.IsMatch(param))
-                GenerateTiebreakerProcedure();
-            else if (free_point_regex.IsMatch(param))
-                GenerateFreePointProcedure(TeamFromLetter(free_point_regex.Match(param).Groups[1].Value));
+            switch (param.ToLowerInvariant())
+            {
+                case "warm1":
+                    GenerateWarmupProcedure(ProcedureTeam.Team1);
+                    break;
+                case "warm2":
+                    GenerateWarmupProcedure(ProcedureTeam.Team2);
+                    break;
+                case "warmw":
+                    GenerateWarmupProcedure(ProcedureTeam.RollWinner);
+                    break;
+                case "warml":
+                    GenerateWarmupProcedure(ProcedureTeam.RollLoser);
+                    break;
+                case "roll":
+                    GenerateRollingProcedure();
+                    break;
+                case "p1":
+                    GeneratePickingProcedure(ProcedureTeam.Team1);
+                    break;
+                case "p2":
+                    GeneratePickingProcedure(ProcedureTeam.Team2);
+                    break;
+                case "pw":
+                    GeneratePickingProcedure(ProcedureTeam.RollWinner);
+                    break;
+                case "pl":
+                    GeneratePickingProcedure(ProcedureTeam.RollLoser);
+                    break;
+                case "b1":
+                    GenerateBanningProcedure(ProcedureTeam.Team1);
+                    break;
+                case "b2":
+                    GenerateBanningProcedure(ProcedureTeam.Team2);
+                    break;
+                case "bw":
+                    GenerateBanningProcedure(ProcedureTeam.RollWinner);
+                    break;
+                case "bl":
+                    GenerateBanningProcedure(ProcedureTeam.RollLoser);
+                    break;
+                case "tb":
+                    GenerateTiebreakerProcedure();
+                    break;
+                case "free1":
+                    GenerateFreePointProcedure(ProcedureTeam.Team1);
+                    break;
+                case "free2":
+                    GenerateFreePointProcedure(ProcedureTeam.Team2);
+                    break;
+                case "freew":
+                    GenerateFreePointProcedure(ProcedureTeam.RollWinner);
+                    break;
+                case "freel":
+                    GenerateFreePointProcedure(ProcedureTeam.RollLoser);
+                    break;
+            }
         }
 
         private void GenerateWarmupProcedure(ProcedureTeam team)
         {
-            Procedures.Add(IsProcedureTeamTeam1(team) ? MatchProcedure.WarmUp1 : MatchProcedure.WarmUp2);
+            var procedure = team == ProcedureTeam.Team1 ? MatchProcedure.WarmUp1
+                : team == ProcedureTeam.Team2 ? MatchProcedure.WarmUp2
+                : team == ProcedureTeam.RollWinner ? MatchProcedure.WarmUpRollWinner
+                : MatchProcedure.WarmUpRollLoser;
+
+            Procedures.Add(procedure);
             Procedures.Add(MatchProcedure.GettingReady);
             Procedures.Add(MatchProcedure.Playing);
         }
@@ -96,12 +139,25 @@ namespace RefereeAssistant3.Main
 
         private void GeneratePickingProcedure(ProcedureTeam team)
         {
-            Procedures.Add(IsProcedureTeamTeam1(team) ? MatchProcedure.Picking1 : MatchProcedure.Picking2);
+            var procedure = team == ProcedureTeam.Team1 ? MatchProcedure.Picking1
+                : team == ProcedureTeam.Team2 ? MatchProcedure.Picking2
+                : team == ProcedureTeam.RollWinner ? MatchProcedure.PickingRollWinner
+                : MatchProcedure.PickingRollLoser;
+
+            Procedures.Add(procedure);
             Procedures.Add(MatchProcedure.GettingReady);
             Procedures.Add(MatchProcedure.Playing);
         }
 
-        private void GenerateBanningProcedure(ProcedureTeam team) => Procedures.Add(IsProcedureTeamTeam1(team) ? MatchProcedure.Banning1 : MatchProcedure.Banning2);
+        private void GenerateBanningProcedure(ProcedureTeam team)
+        {
+            var procedure = team == ProcedureTeam.Team1 ? MatchProcedure.Banning1
+                : team == ProcedureTeam.Team2 ? MatchProcedure.Banning2
+                : team == ProcedureTeam.RollWinner ? MatchProcedure.BanningRollWinner
+                : MatchProcedure.BanningRollLoser;
+
+            Procedures.Add(procedure);
+        }
 
         private void GenerateTiebreakerProcedure()
         {
@@ -111,30 +167,6 @@ namespace RefereeAssistant3.Main
         }
 
         private void GenerateFreePointProcedure(ProcedureTeam team) => Procedures.Add(IsProcedureTeamTeam1(team) ? MatchProcedure.FreePoint1 : MatchProcedure.FreePoint2);
-
-        private static readonly Regex warmup_regex = new Regex(@"^warm(1|2|w|l)$");
-        private static readonly Regex roll_regex = new Regex(@"^roll$");
-        private static readonly Regex pick_regex = new Regex(@"^p(1|2|w|l)$");
-        private static readonly Regex ban_regex = new Regex(@"^b(1|2|w|l)$");
-        private static readonly Regex tiebreaker_regex = new Regex(@"^tb$");
-        private static readonly Regex free_point_regex = new Regex(@"^free(1|2|w|l)$");
-
-        private ProcedureTeam TeamFromLetter(string letter)
-        {
-            switch (letter)
-            {
-                case "w":
-                    return ProcedureTeam.RollWinner;
-                case "l":
-                    return ProcedureTeam.RollLoser;
-                case "1":
-                    return ProcedureTeam.Team1;
-                case "2":
-                    return ProcedureTeam.Team2;
-                default:
-                    throw new Exception();
-            }
-        }
 
         private bool IsProcedureTeamTeam1(ProcedureTeam procedureTeam)
         {
@@ -153,7 +185,10 @@ namespace RefereeAssistant3.Main
             if (CurrentProcedure == MatchProcedure.WarmUp1 || CurrentProcedure == MatchProcedure.WarmUp2)
                 return GoToNextProcedure();
             if (CurrentProcedure == MatchProcedure.Rolling && RollWinner != null)
+            {
+                GenerateMatchProcedures();
                 return GoToNextProcedure();
+            }
             if (SelectedMap != null)
             {
                 if (CurrentProcedure == MatchProcedure.Banning1)
@@ -199,30 +234,5 @@ namespace RefereeAssistant3.Main
             if (CurrentProcedure == MatchProcedure.Banning1 || CurrentProcedure == MatchProcedure.Banning2)
                 SelectedMap = null;
         }
-    }
-
-    public enum MatchProcedure
-    {
-        SettingUp = 0,
-        WarmUp1 = 1,
-        WarmUp2 = 2,
-        Rolling = 3,
-        Banning1 = 4,
-        Banning2 = 5,
-        Picking1 = 6,
-        Picking2 = 7,
-        GettingReady = 8,
-        TieBreaker = 9,
-        Playing = 10,
-        FreePoint1 = 11,
-        FreePoint2 = 12,
-    }
-
-    public enum ProcedureTeam
-    {
-        Team1 = 0,
-        Team2 = 1,
-        RollWinner = 2,
-        RollLoser = 3
     }
 }
---
+++ RefereeAssistant3.Main/ProcedureUtilities.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+
+namespace RefereeAssistant3.Main
+{
+    public class ProcedureUtilities
+    {
+        private readonly Match match;
+
+        public ProcedureUtilities(Match match) => this.match = match;
+
+        private string rollWinner => match.RollWinner?.TeamName ?? "roll winner";
+        private string rollLoser => match.RollLoser?.TeamName ?? "roll loser";
+
+        private Dictionary<MatchProcedure, string> readableMatchStateDictionary => new Dictionary<MatchProcedure, string>
+        {
+            { MatchProcedure.SettingUp, "Setting up the match" },
+
+            { MatchProcedure.WarmUp1, $"Warmup by {match.Team1}" },
+            { MatchProcedure.WarmUp2, $"Warmup by {match.Team2}" },
+
+            { MatchProcedure.WarmUpRollWinner, $"Warmup by {rollWinner}" },
+            { MatchProcedure.WarmUpRollLoser, $"Warmup by {rollLoser}" },
+
+            { MatchProcedure.Rolling, $"Teams rolling" },
+
+            { MatchProcedure.Banning1, $"{match.Team1} banning" },
+            { MatchProcedure.Banning2, $"{match.Team2} banning" },
+            { MatchProcedure.BanningRollWinner, $"{rollWinner} banning" },
+            { MatchProcedure.BanningRollLoser, $"{rollLoser} banning" },
+
+            { MatchProcedure.Picking1, $"{match.Team1} picking" },
+            { MatchProcedure.Picking2, $"{match.Team2} picking" },
+            { MatchProcedure.PickingRollWinner, $"{rollWinner} picking" },
+            { MatchProcedure.PickingRollLoser, $"{rollLoser} picking" },
+
+            { MatchProcedure.GettingReady, "Players getting ready" },
+
+            { MatchProcedure.TieBreaker, "Tiebreaker!" },
+
+            { MatchProcedure.Playing, $"Playing {match.SelectedMap?.MapCode}: {match.SelectedMap?.DisplayName}" },
+
+            { MatchProcedure.FreePoint1, $"{match.Team1} receives a free point" },
+            { MatchProcedure.FreePoint2, $"{match.Team2} receives a free point" },
+            { MatchProcedure.FreePointRollWinner, $"{rollWinner} receives a free point" },
+            { MatchProcedure.FreePointRollLoser, $"{rollLoser} receives a free point" }
+        };
+
+        public string GetProcedureDescription(MatchProcedure procedure) => readableMatchStateDictionary.GetValueOrDefault(procedure) ?? procedure.ToString();
+    }
+
+    public enum MatchProcedure
+    {
+        SettingUp = 0,
+        WarmUp1 = 1,
+        WarmUp2 = 2,
+        Rolling = 3,
+        Banning1 = 4,
+        Banning2 = 5,
+        Picking1 = 6,
+        Picking2 = 7,
+        GettingReady = 8,
+        TieBreaker = 9,
+        Playing = 10,
+        FreePoint1 = 11,
+        FreePoint2 = 12,
+        BanningRollWinner = 13,
+        BanningRollLoser = 14,
+        PickingRollWinner = 15,
+        PickingRollLoser = 16,
+        WarmUpRollWinner = 17,
+        WarmUpRollLoser = 18,
+        FreePointRollWinner = 19,
+        FreePointRollLoser = 20
+    }
+
+    public enum ProcedureTeam
+    {
+        Team1 = 0,
+        Team2 = 1,
+        RollWinner = 2,
+        RollLoser = 3
+    }
+}
