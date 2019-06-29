698b7bbee1386f353c4106260d18ad3815ca59d5
Hōtarō Oreki <TheZjarek@gmail.com>
2019-06-29 21:17:59 +0200

    Improve RA3Button

--- RefereeAssistant3.Visual/RA3Button.cs
+++ RefereeAssistant3.Visual/RA3Button.cs
@@ -1,25 +1,55 @@
 ﻿using osu.Framework.Bindables;
+using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
-using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Input.Events;
 using osuTK.Graphics;
 
 namespace RefereeAssistant3.Visual
 {
-    public class RA3Button : Button
+    public class RA3Button : ClickableContainer
     {
+        private const float hover_alpha = 1f;
         private readonly Box hoverOverlay;
+        private readonly Box background;
+
+        public TextFlowContainer TextFlow { get; }
+        public string Text { set => TextFlow.Text = value ?? ""; }
+
+        public Color4 BackgroundColour
+        {
+            get => background.Colour;
+            set
+            {
+                background.Colour = value;
+                hoverOverlay.Colour = value.Lighten(0.5f);
+            }
+        }
 
         public RA3Button()
         {
             Height = Style.COMPONENTS_HEIGHT;
-            Add(hoverOverlay = new Box
+            Children = new Drawable[]
             {
-                RelativeSizeAxes = Axes.Both,
-                Colour = Color4.White,
-                Alpha = 0
-            });
+                background = new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                },
+                hoverOverlay = new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Alpha = 0
+                },
+                TextFlow = new TextFlowContainer
+                {
+                    AutoSizeAxes = Axes.Both,
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    TextAnchor = Anchor.TopCentre
+                }
+            };
+            BackgroundColour = FrameworkColour.Green;
             Enabled.BindValueChanged(OnEnabledValueChanged, true);
         }
 
@@ -29,13 +59,13 @@ namespace RefereeAssistant3.Visual
             if (!Enabled.Value)
                 hoverOverlay.FadeOut(200, Easing.OutCubic);
             else if (IsHovered)
-                hoverOverlay.FadeTo(0.2f, 120, Easing.OutCubic);
+                hoverOverlay.FadeTo(hover_alpha, 120, Easing.OutCubic);
         }
 
         protected override bool OnHover(HoverEvent e)
         {
             if (Enabled.Value)
-                hoverOverlay.FadeTo(0.2f, 120, Easing.OutCubic);
+                hoverOverlay.FadeTo(hover_alpha, 120, Easing.OutCubic);
             return true;
         }
 
